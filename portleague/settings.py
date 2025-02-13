1caec6a91298bebcb0e6acd3f731a8c9a6a14f52
viliiiiii <vilipievoda@gmail.com>
2025-02-13 05:08:16 +0000

    Fixed STATIC_ROOT for Render deployment

--- portleague/settings.py
+++ portleague/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Ensure STATIC_ROOT is defined
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
